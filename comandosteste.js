4eff0b1f1851c72d82d8012f6ca1839375966264
Davi Ximenes <dropperdavi@gmail.comm>
2021-04-13 18:04:30 -0300

    anotações2

--- comandosteste.js
+++ comandosteste.js
@@ -1,1 +1,9 @@
 //<h1 onclick="this.innerHTML = 'Ooops!'">Click on this text!</h1>
+
+/*<h1 onclick="changeText(this)">Click on this text!</h1>
+
+<script>
+function changeText(id) {
+  id.innerHTML = "Ooops!";
+}
+</script>*/
