c333b7f25fe99e70f1b8d6284076312f405a36f8
Luiz Miguel <fait0n2525@gmail>
2021-04-13 20:32:22 -0300

    Atribuindo os botões para a tela

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 var teste = new Audio();
+teste.src = 'click.mp3';
+
+var teste = new Audio();
 teste.src = 'click.mp3'
+
+var n1 = 0;
+function display(num) {
+    if (document.form.txt.value == 0) {
+        document.form.txt.value = "";
+        document.form.txt.value = document.form.txt.value + num
+    } else {
+        document.form.txt.value = document.form.txt.value + num
+    }
+
+}
+
+function clear() {
+    document.form.txt.value = "";
+}
+
+function plus() {
+    n1 = document.form.txt.value;
+    console.log(n1);
+}
+
