b0281eb61c16c6d93b4761715a3211233e0ff37b
Davi Ximenes <dropperdavi@gmail.comm>
2021-04-13 19:23:41 -0300

    anotações4

--- eventosteste.js
+++ eventosteste.js
@@ -1,5 +1,7 @@
 //<h1 onclick="this.innerHTML = 'Ooops!'">Click on this text!</h1>
 
+//-----------------------------------------------------------------------------
+
 /*<h1 onclick="changeText(this)">Click on this text!</h1>
 
 <script>
@@ -7,3 +9,23 @@ function changeText(id) {
   id.innerHTML = "Ooops!";
 }
 </script>*/
+
+//------------------------------------------------------------------------------
+
+/*
+<p>Click the button to display the date.</p>
+
+<button onclick="displayDate()">The time is?</button>
+
+<script>
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
+</script>
+
+<p id="demo"></p>
+*/
+
+//-----------------------------------------------------------------------------
+
+// id="sept" onmousedown="seven()"
