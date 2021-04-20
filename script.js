e0f3deae374a72215acd554d2b99abf83885c327
Luiz Miguel <fait0n2525@gmail>
2021-04-19 22:21:26 -0300

    Update 1

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 var teste = new Audio();
 teste.src = 'click.mp3';
 
-
-/*var n1 = 0;
-function deletar() {
-    document.form.txt.value = "";
-    document.form.txt.value = 0;
-}
-
-function plus() {
-    n1 = document.form.txt.value;
-    console.log(n1);
-}
-function limpar() {
-    document.getElementById('resultado').innerHTML = " ";
-}
-function apagar() {
-    var voltar = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = voltar.substring(0, voltar.length - 1);
-}
-function calcular() {
-    var calcula = document.getElementById('resultado').innerHTML;
-    if (calcula) {
-        document.getElementById('resultado').innerHTML = eval(calcula);
-    }
-    else {
-        document.getElementById('resultado').innerHTML = "Erro";
-    }
-}
-*/
 var button = document.querySelectorAll(".button");
 var operationButton = document.querySelectorAll(".operation");
 var screen = document.form.txt;
@@ -36,13 +8,11 @@ var resultado;
 var valor;
 var block = false;
 var array = [];
+
 button.forEach(function (button) {
     button.addEventListener("click", function (event) {
-        display();
         array = screen.value
-        console.log(array[array.length - 1])
-        console.log(screen.value.length)
-
+        display();
     })
 })
 
@@ -88,7 +58,7 @@ function deleteC() {
     }
 }
 
-//apagar de um por um 
+//Delete one by one
 operation.forEach(function (operation) {
     operation.addEventListener("click", function (event) {
         deletee();
@@ -98,7 +68,7 @@ function deletee() {
     screen.value;
     screen.value = screen.value.substring(0, screen.value.length - 1);
 }
-//operação para fazer as contas 
+//Operation for making a account
 operation.forEach(function (operation) {
     operation.addEventListener("click", function (event) {
         conta();
